0f3f71bc6fd604d7886983398682067e86562d84
Kei Sugano <kei.sugano@h-t.co.jp>
2020-08-08 15:04:56 +0900

    bunch of features added

--- sugamd.py
+++ sugamd.py
@@ -15,9 +15,11 @@ BLUE      = '\033[94m'
 GREEN     = '\033[92m'
 YELLOW    = '\033[93m'
 RED       = '\033[91m'
+BLINK     = '\033[5m'
 BOLD      = '\033[1m'
 UNDERLINE = '\033[4m'
 NORMAL    = '\033[0m'
+LIGHTBLUE = '\033[104m'
 
 #
 # 
@@ -26,8 +28,47 @@ f = open(file_name, 'r')
 content = f.read().split('\n')
 f.close()
 
+block  = False
+indent = '' 
+
+hrline = '\n---------------------------------------------------------------------------------------------------\n'
+
 for line in content:
-    if(re.match("^#[^#].*", line)):
-        print(BOLD + line[1:].strip())
+    painted = False
+
+    if(re.match("[^`]*```[^`]*", line)):
+        block = not block
+        line = hrline
+
+    if(not block):
+        if(re.match("^#[^#].*", line)):
+            line = "\n" + BOLD + line.strip()[1:].strip()
+            painted = True
+            indent  = '' 
+
+        if(re.match("^##[^#].*", line)):
+            line = "\n  " + BOLD + line.strip()[2:].strip()
+            painted = True
+            indent  = '  ' 
+
+        if(re.match("^###[^#].*", line)):
+            line = "\n    " + BOLD + line.strip()[4:].strip()
+            painted = True
+            indent  = '    ' 
+
+        if(re.match("^\s*\*\s*.*", line)):
+            line = indent + ' - ' + line.strip()[1:]
+            painted = True
+
+        if(not painted and line != hrline):
+            line = NORMAL + indent + line.strip()
+
+        line = re.sub('`([^`]+)`', BOLD + '\\1' + NORMAL, line)
+        # line = re.sub('(\(http.*\))', BOLD + '\\1' + NORMAL, line).replace('(', '').replace(')', '')
+
+    if(block and not hrline):
+        line = BOLD + line
+
+    print(line)
 
 print(NORMAL + '---')
