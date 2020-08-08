7d597a8b8828200fad79e90b583d76eed331f8d2
Kei Sugano <kei.sugano@h-t.co.jp>
2020-08-08 16:14:01 +0900

    table description

--- sugamd.py
+++ sugamd.py
@@ -30,8 +30,14 @@ f.close()
 
 block  = False
 indent = '' 
+table_mode = False
 
 hrline = '\n---------------------------------------------------------------------------------------------------\n'
+
+records = []
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
 
 for line in content:
     painted = False
@@ -68,14 +74,63 @@ for line in content:
         if(not painted and line != hrline):
             line = NORMAL + indent + line.strip()
 
-        line = re.sub('`([^`]+)`', BOLD + '\\1' + NORMAL, line)
-        line = re.sub('\*\*([^*]+)\*\*', BOLD + '\\1' + NORMAL, line)
-        line = re.sub('\*([^*]+)\*', BOLD + '\\1' + NORMAL, line)
+        if(re.match(r'.*\|', line)):
+            table_mode = True
+            xs = line.split("|")[1:]
+            xs = [x.strip() for x in xs]
+            records.append(xs)
+        elif(table_mode == True):
+            table_mode = False
+
+            col_max_sizes = []
+            for col in records[0]:
+                col_max_sizes.append(0)
+                
+            for row in records:
+                i = 0
+                for col in row:
+                    if(col_max_sizes[i] < len(col)):
+                        col_max_sizes[i] = len(col)
+                    i = i + 1
+            
+            records_cp = []
+            rlflag = False
+            for row in records:
+                xs = []
+                i = 0
+                for col in row:
+                    if(len(col) < col_max_sizes[i]):
+                        for j in range(col_max_sizes[i] - len(col)):
+                            if(re.match("^\-+$", col)):
+                                if(rlflag):
+                                    col = '-' + col
+                                else:
+                                    col = col + '-'
+                            else:
+                                if(rlflag):
+                                    col = ' ' + col
+                                else:
+                                    col = col + ' '
+                            rlflag = not rlflag
+                    xs.append(col) 
+                    i = i + 1
+                records_cp.append(xs)
+            
+            for row in records_cp:
+                for col in row:
+                    print(col + '|', end = '')
+                print("")
+
+        if(not table_mode):
+            line = re.sub('`([^`]+)`', BOLD + '\\1' + NORMAL, line)
+            line = re.sub('\*\*([^*]+)\*\*', BOLD + '\\1' + NORMAL, line)
+            line = re.sub('\*([^*]+)\*', BOLD + '\\1' + NORMAL, line)
         # line = re.sub('(\(http.*\))', BOLD + '\\1' + NORMAL, line).replace('(', '').replace(')', '')
 
     if(block and not hrline):
         line = BOLD + line
 
-    print(line)
+    if(not table_mode):
+        print(line)
 
 print(NORMAL + '---')
